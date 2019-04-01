b06bdfec9107d165bdbebb9321e185d615828883
Danica <danicadjordjevic540@gmail.com>
2019-04-01 15:15:15 +0200

    navbar dodat

---
+++ RWA_Danica/src/default_page_functions.js
@@ -0,0 +1,7 @@
+
+export function default_view(sign_in_form,news_view,error_div,profile_link){
+    news_view.style.display="flex";
+    sign_in_form.style.display="none";
+    error_div.style.display="none";
+    profile_link.style.display="none";
+}
--- RWA_Danica/src/index.js
+++ RWA_Danica/src/index.js
@@ -1,4 +1,5 @@
 import {validate_user,change_to_sign_up_mode,is_sign_in_mode,sign_in_view} from "./log-in-functions";
+import {default_view} from "./default_page_functions";
 import {interval, range, Observable,fromEvent} from "rxjs";
 import {take,filter,map,scan} from "rxjs/operators";
 
@@ -11,8 +12,12 @@ let sign_in_button=document.getElementById("sign-in-button");
 let sign_up_link=document.getElementById("sign-up-link");
 let sign_in_label=document.getElementById("sign-in-label");
 let sign_up_div=document.getElementById("sign-up-div");
+let sign_in_link=document.getElementById("sign-in-link");
+let profile_link=document.getElementById("profile_link");
 
-sign_in_view(sign_in_form,news_view,error_div);
+default_view(sign_in_form,news_view,error_div,profile_link);
+
+//sign_in_view(sign_in_form,news_view,error_div);
 
 // data source is the click event on the button, it goes from the data source to the function we pass to the subscribe method, it's like date travels through a funnel
 
@@ -29,6 +34,11 @@ fromEvent(sign_in_button, 'click')
 fromEvent(sign_up_link, 'click')
   .subscribe(() => {
     change_to_sign_up_mode(sign_in_label,sign_in_button,sign_up_div,error_div);
+});
+
+fromEvent(sign_in_link, 'click')
+  .subscribe(() => {
+    sign_in_view(sign_in_form,news_view,error_div)
 });
 
 
